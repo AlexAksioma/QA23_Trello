85628ccb7b768ff2f09d406cd9c007979d7cd46d
AlexAksioma <alexmedqwerty@gmail.com>
2023-12-26 18:58:36 +0200

    added manager, tests

--- src/main/java/manager/HelperLogin.java
+++ src/main/java/manager/HelperLogin.java
@@ -1,2 +1,9 @@
-package manager;public class HelperLogin {
+package manager;
+
+import org.openqa.selenium.WebDriver;
+
+public class HelperLogin extends HelperBase{
+    public HelperLogin(WebDriver driver) {
+        super(driver);
+    }
 }
