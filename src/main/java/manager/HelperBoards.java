717a222ffd3d5a33f835e2b2cb933a7b86bd911e
AlexAksioma <alexmedqwerty@gmail.com>
2024-01-16 19:55:49 +0200

    added deleteBoard + after method

--- src/main/java/manager/HelperBoards.java
+++ src/main/java/manager/HelperBoards.java
@@ -4,28 +4,70 @@ import models.BoardDTO;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HelperBoards extends HelperBase{
+public class HelperBoards extends HelperBase {
     public HelperBoards(WebDriver driver) {
         super(driver);
     }
+
     By buttonCreateNewBoard = By.xpath("//li[@data-testid='create-board-tile']//span");
     By inputBoardTitle = By.xpath("//input[@data-testid='create-board-title-input']");
     By buttonCreateBoard = By.xpath("//button[@data-testid='create-board-submit-button']");
     By boardNameDisplay = By.xpath("//h1[@data-testid='board-name-display']");
     By buttonBoards = By.cssSelector("a[data-testid='open-boards-link']");
+    //===================================
+    By buttonDots = By.cssSelector("button[aria-label='Show menu']");
+
+    By buttonCloseBoard =
+            By.cssSelector("a[class=\"board-menu-navigation-item-link board-menu-navigation-item-link-v2 js-close-board\"]");
+
+    By buttonCloseConfirm = By.cssSelector("input[value='Close']");
+
+    By buttonDeleteBoard = By.cssSelector("button[data-testid='close-board-delete-board-button']");
+
+    By buttonDeleteConfirm = By.cssSelector("button[data-testid='close-board-delete-board-confirm-button']");
+
+    By deleteMessage = By.cssSelector("div[id='FlagGroup']");
+    By deleteMessageText = By.xpath("//span[text()='Board deleted.']");
 
     public void createNewBoard(BoardDTO board) {
         clickBase(buttonCreateNewBoard);
         typeBase(inputBoardTitle, board.getBoardTitle());
-        pause(3);
+        pause(5);
         clickBase(buttonCreateBoard);
     }
 
-    public boolean isBoardTitlePresent(String text){
+    public boolean isBoardTitlePresent(String text) {
         return isTextInElementEquals(boardNameDisplay, text);
     }
 
-    public void clickButtonBoards(){
+    public void clickButtonBoards() {
         clickBase(buttonBoards);
+    }
+
+    public void deleteBoard(String boardTitle) {
+        clickBoardOnTitle(boardTitle);
+        driver.navigate().refresh();
+        clickBase(buttonDots);
+        clickBase(buttonCloseBoard);
+        clickBase(buttonCloseConfirm);
+        clickBase(buttonDeleteBoard);
+        clickBase(buttonDeleteConfirm);
+    }
+
+    public void clickBoardOnTitle(String boardTitle) {
+        String xPathBoardTitle = "//div[@title='" + boardTitle + "']";
+        clickBase(By.xpath(xPathBoardTitle));
+
+    }
+
+    public boolean isDeleteMessagePresent() {
+        return isTextInElementEquals(deleteMessage, "Board deleted.");
+    }
+    public boolean isTextMessagePresentByText(){
+        return isElementPresent(deleteMessageText);
+    }
+
+    public boolean isElementPresent_buttonBoards() {
+        return isElementPresent(buttonBoards);
     }
 }
--- src/test/java/tests/BoardTests.java
+++ src/test/java/tests/BoardTests.java
@@ -5,7 +5,6 @@ import models.UserDTO;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Random;
@@ -22,9 +21,9 @@ public class BoardTests extends TestBase {
 
     @Test
     public void addNewBoardPositiveTest() {
-        int i = new Random().nextInt(1000)+1000;
+        int i = new Random().nextInt(1000) + 1000;
         BoardDTO board = BoardDTO.builder()
-                .boardTitle("board"+i)
+                .boardTitle("board" + i)
                 .build();
         app.getHelperBoards().createNewBoard(board);
         Assert.assertTrue(app.getHelperBoards().isBoardTitlePresent(board.getBoardTitle()));
@@ -32,11 +31,35 @@ public class BoardTests extends TestBase {
 
     @Test
     public void deleteBoardPositiveTest() {
-        System.out.println("second");
+        int i = new Random().nextInt(1000) + 1000;
+        BoardDTO board = BoardDTO.builder()
+                .boardTitle("board_del" + i)
+                .build();
+        app.getHelperBoards().createNewBoard(board);
+        app.getHelperBoards().clickButtonBoards();
+
+        app.getHelperBoards().deleteBoard(board.getBoardTitle());
+        Assert.assertTrue(app.getHelperBoards().isTextMessagePresentByText());
+
+    }
+
+    @Test
+    public void deleteBoardPositiveTest1() {
+        int i = new Random().nextInt(1000) + 1000;
+        BoardDTO board = BoardDTO.builder()
+                .boardTitle("board_del" + i)
+                .build();
+        app.getHelperBoards().createNewBoard(board);
+        app.getHelperBoards().clickButtonBoards();
+
+        app.getHelperBoards().deleteBoard(board.getBoardTitle());
+        Assert.assertTrue(app.getHelperBoards().isTextMessagePresentByText());
+
     }
 
     @AfterMethod
-    public void afterMethod(){
-        app.getHelperBoards().clickButtonBoards();
+    public void afterMethod() {
+        if (app.getHelperBoards().isElementPresent_buttonBoards())
+            app.getHelperBoards().clickButtonBoards();
     }
 }
